5eb26adbaaac60d26d3084170cccb47bc19be63a
jgiel <joshua.giel@gmail.com>
2024-10-24 22:02:07 -0700

    separate constants and errors

---
+++ constants.py
@@ -0,0 +1,7 @@
+import logging
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"
+}
+
+logger = logging.getLogger(__name__)
